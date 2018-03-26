6e5385791b2649c320169ba4a5fdbd78ddfad671
Gilmar Lima <gilmaslima@gmail.com>
2018-03-25 21:03:14 -0300

    adicionada rota de sumario no backend

--- CursoFramesWeb/backend/config/routes.js
+++ CursoFramesWeb/backend/config/routes.js
@@ -4,4 +4,11 @@ module.exports = function(server) {
   //API routes
   const router = express.Router();
   server.use("/api", router);
+
+  // rotas da API
+  const billingCycleService = require("../api/billingCycle/billingCycleService");
+  billingCycleService.register(router, "/billingCycles");
+
+  const billingCycleSummaryService = require("../api/billingCycleSummary/billingCycleSummaryService");
+  router.route("/billingSummary").get(billingCycleSummaryService.getSummary);
 };
