04a7056dab1524180c0c50f2048f70c82a7fc147
Gilmar Lima <gilmaslima@gmail.com>
2018-04-01 23:02:33 -0300

    adicionado projeto front-end

--- CursoFramesWeb/backend/api/billingCycle/billingCycleService.js
+++ CursoFramesWeb/backend/api/billingCycle/billingCycleService.js
@@ -1,17 +1,34 @@
-const BillingCycle = require('./billingCycle')
+const BillingCycle = require("./billingCycle");
+const _ = require("lodash");
 
-BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.methods(["get", "post", "put", "delete"]);
+BillingCycle.updateOptions({ new: true, runValidators: true });
 
-BillingCycle.route('count', function(req, res, next){
+BillingCycle.after("post", sendErrorsOrNext).after("put", sendErrorsOrNext);
 
-    BillingCycle.count(function(error, value){
-        if(error){
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json({value})
-        }
-    })
-})
+function sendErrorsOrNext(req, res, next) {
+  const bundle = res.locals.bundle;
+  if (bundle.errors) {
+    var errors = parseError(bundle.errors);
+    res.status(500).json({ errors });
+  } else {
+    next();
+  }
+}
 
-module.exports = BillingCycle
+function parseError(nodeRestfulErrors) {
+  const errors = [];
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message));
+  return errors;
+}
+BillingCycle.route("count", function(req, res, next) {
+  BillingCycle.count(function(error, value) {
+    if (error) {
+      res.status(500).json({ errors: [error] });
+    } else {
+      res.json({ value });
+    }
+  });
+});
+
+module.exports = BillingCycle;
