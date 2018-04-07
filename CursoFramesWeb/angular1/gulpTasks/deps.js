418d65f14204dd4fc5cd04ace8ba5e2d5241a4e1
Gilmar Lima <gilmaslima@gmail.com>
2018-04-07 13:54:31 -0300

    Adicionados arquivos html

--- CursoFramesWeb/angular1/gulpTasks/deps.js
+++ CursoFramesWeb/angular1/gulpTasks/deps.js
@@ -11,7 +11,7 @@ gulp.task("deps.js", () => {
       "node_modules/angular/angular.min.js",
       "node_modules/angular-ui-router/release/angular-ui-router.min.js",
       "node_modules/angular-animate/angular-animate.min.js",
-      "node_modules/angular-toastr/dist/angular-toastr.tpls.js",
+      "node_modules/angular-toastr/dist/angular-toastr.tpls.min.js",
       "node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js",
       "node_modules/admin-lte/bootstrap/js/bootstrap.min.js",
       "node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js",
@@ -21,7 +21,6 @@ gulp.task("deps.js", () => {
     .pipe(concat("deps.min.js"))
     .pipe(gulp.dest("public/assets/js"));
 });
-
 gulp.task("deps.css", () => {
   return gulp
     .src([
@@ -31,8 +30,8 @@ gulp.task("deps.css", () => {
       "node_modules/admin-lte/dist/css/AdminLTE.min.css",
       "node_modules/admin-lte/dist/css/skins/_all-skins.min.css"
     ])
-    .pipe(uglifycss())
-    .pipe(concat("deps.min.css"))
+    .pipe(uglifycss({ uglyComments: true }))
+    .pipe(concat("deps.min.js"))
     .pipe(gulp.dest("public/assets/css"));
 });
 
