dbb1bd2d59b63c1ae3707dcc2d084fb2b63436b3
Gilmar Lima <gilmaslima@gmail.com>
2018-04-08 23:30:53 -0300

    refactory dashnoard controller

--- CursoFramesWeb/angular1/app/dashboard/dashboardController.js
+++ CursoFramesWeb/angular1/app/dashboard/dashboardController.js
@@ -5,7 +5,8 @@ angular
 function DashboardController($scope, $http) {
   $scope.getSummary = function() {
     const url = "http://localhost:3003/api/billingSummary";
-    $http.get(url).success(function({ credit = 0, debt = 0 }) {
+    $http.get(url).then(function(response) {
+      const { credit = 0, debt = 0 } = response.data
       $scope.credit = credit;
       $scope.debt = debt;
       $scope.total = credit - debt;
